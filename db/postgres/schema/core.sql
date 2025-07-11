2d1d4389d1f0154c7ef1508a4b2b56b759aab86a
dann3256 <dambara.riki790@mail.kyutech.jp>
2025-07-11 22:23:53 +0900

    fic core.sql

--- db/postgres/schema/core.sql
+++ db/postgres/schema/core.sql
@@ -1,7 +1,28 @@
+CREATE TABLE banks (
+  id INT PRIMARY KEY,
+  bank_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE deadlines (
+  id INT PRIMARY KEY,
+  payment_date VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE companies (
+  id SERIAL PRIMARY KEY,
+  bank_id INT NOT NULL REFERENCES banks (id),
+  payment_deadline_id INT NOT NULL REFERENCES deadlines (id),
+  company_name VARCHAR(255) NOT NULL,
+  phone_num INT NOT NULL,
+  payment_amount INT NOT NULL,
+  payment_limit INT NOT NULL,
+  create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE users (
-  id INT,
+  id SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password_hash VARCHAR(255) NOT NULL,
@@ -11,46 +32,18 @@ CREATE TABLE users (
 CREATE INDEX idx_users_email ON users (email);
 
 CREATE TABLE bills (
-  id INT,
+  id SERIAL PRIMARY KEY,
   user_id INT NOT NULL REFERENCES users (id),
   company_id INT NOT NULL REFERENCES companies (id),
-  amount INT NOT NULL CHECK (amount >= 0),
-  due_date DATE NOT NULL,
   is_paid BOOLEAN DEFAULT FALSE NOT NULL,
-  paid_at TIMESTAMPTZ NULL,
+  paid_at TIMESTAMPTZ,
   memo TEXT,
   created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   deleted_at TIMESTAMPTZ NULL
 );
 
-CREATE INDEX idx_bills_user_id ON bills (user_id);
-
-CREATE INDEX idx_bills_due_date ON bills (due_date);
-
-CREATE INDEX idx_bills_unpaid_due_date ON bills (due_date)
+CREATE INDEX idx_bills_unpaid_created_at ON bills (created_at)
 WHERE
   is_paid = FALSE
   AND deleted_at IS NULL;
-
--- paid automatic update
-CREATE OR REPLACE FUNCTION SET_PAID_AT () RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.is_paid = TRUE AND OLD.is_paid = FALSE THEN
-        NEW.paid_at = CURRENT_TIMESTAMP;
-    ELSIF NEW.is_paid = FALSE THEN
-        NEW.paid_at = NULL;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER set_bills_paid_at BEFORE
-UPDATE ON bills FOR EACH ROW
-EXECUTE FUNCTION SET_PAID_AT ();
-
-CREATE TABLE companies (
-  id INT,
-  company_name VARCHAR(255) NOT NULL,
-  phone_num INT NOT NULL
-);
